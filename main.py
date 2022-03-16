ca21eadbb01222e294f3aa2aa735a63b30948822
jara <jam.laureijssen@student.han.nl>
2022-03-16 09:46:19 +0100

    Bestanden toegevoegd

---
+++ main.py
@@ -0,0 +1,15 @@
+# Created By  : Eva Nijhuis, Jara Laureijssen, Rico van der lande en
+# Ward Strik
+# Created Date: 16-03-2022
+# version ='1.0'
+# ---------------------------------------------------------------------
+"""Vind ORF's in ingevoerde bestanden. De gevonden ORF's worden
+vervolgens geannoteerd of het functionele genen zijn"""
+
+
+def main():
+    print("hallo")
+
+
+if __name__ == '__main__':
+    main()
