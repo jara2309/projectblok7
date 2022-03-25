c5b8bf5b152f878fa772a0105bf66d188ccc6a76
NietWard <71081209+NietWard@users.noreply.github.com>
2022-03-25 10:09:33 +0100

    Class gemaakt

--- BestandLezen.py
+++ BestandLezen.py
@@ -1,35 +1,35 @@
-def openfasta(path):
-    """ Opent file, voegt headers toe aan headerarray
-    en voegt sequenties toe aan sequentie array
-    :param path: Bestandsnaam / bestandslocatie
-    :return:seqs: Array met sequenties
-            headers: Array met headers
-    """
-    seq = ""
-    seqs = []
-    headers = []
-    file = open(path)
-    x=0
-    for line in file:
-        if (">" in line): #Controleer of regel header is
-            if x>0:
-                seqs.append(seq) #Sequentie toevoegen aan array seqs
-            headers.append(line.replace("\n", ""))  # Headers toevoegen aan array headers
-            seq = "" #Sequentie leegmaken voor nieuwe seq
-            x+=1
-        else:
-            seq += line.replace("\n", "") #Enters weghalen en Seqbuilder
-    if seq != "":
-        seqs.append(seq) #Voeg laatse sequentie toe
-    file.close()
+class BestandLezen:
+    def openfasta(path):
+        """ Opent file, voegt headers toe aan headerarray
+        en voegt sequenties toe aan sequentie array
+        :param path: Bestandsnaam / bestandslocatie
+        :return:seqs: Array met sequenties
+                headers: Array met headers
+        """
+        seq = ""
+        seqs = []
+        headers = []
+        file = open(path)
+        x=0
+        for line in file:
+            if (">" in line): #Controleer of regel header is
+                if x>0:
+                    seqs.append(seq) #Sequentie toevoegen aan array seqs
+                headers.append(line.replace("\n", ""))  # Headers toevoegen aan array headers
+                seq = "" #Sequentie leegmaken voor nieuwe seq
+                x+=1
+            else:
+                seq += line.replace("\n", "") #Enters weghalen en Seqbuilder
+        if seq != "":
+            seqs.append(seq) #Voeg laatse sequentie toe
+        file.close()
+        return seqs, headers
 
-    return seqs, headers
 
-
-if __name__ == '__main__':
-    try:
-        s, h = openfasta("SD.fa")
-        if not s:
-            print("Warning: No sequences found")
-    except FileNotFoundError:
-        print("File not found, check filepath")
+    if __name__ == '__main__':
+        try:
+            s, h = openfasta("SD.fa")
+            if not s:
+                print("Warning: No sequences found")
+        except FileNotFoundError:
+            print("File not found, check filepath")
